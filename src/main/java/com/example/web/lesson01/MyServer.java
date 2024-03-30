22bc883634d169cd9efc5e78313a7871b23d457c
gausszhou <gausszhou@qq.com>
2024-03-30 11:06:20 +0800

    lesson02

--- src/main/java/com/example/web/lesson01/MyServer.java
+++ src/main/java/com/example/web/lesson01/MyServer.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * http://localhost:8080
+ */
 public class MyServer {
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
         System.out.println("[debug] server is running");
-        for (; ; ) {
+        while (true) {
             Socket sock = serverSocket.accept();
             System.out.println("[debug] connected from" + sock.getRemoteSocketAddress());
             Thread t = new Thread(new MyServerHandler(sock));
